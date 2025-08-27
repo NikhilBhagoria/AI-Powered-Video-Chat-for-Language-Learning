971a0388a6f6a115a8425a30e429b0f144b9c5a5
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-08-27 23:31:54 +0530

    Add BE: Chat New Routes

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { initiateChat } = require('../controllers/chatController');
 const { authenticateToken } = require('../middleware/auth');
+const {
+  getMessages,
+  sendMessage,
+  deleteMessage,
+  markAsRead
+} = require('../controllers/messageController');
 
-router.post('/initiate', authenticateToken, initiateChat);
+// Message routes
+router.get('/:chatId/messages', authenticateToken, getMessages);
+router.post('/:chatId/messages', authenticateToken, sendMessage);
+router.delete('/:chatId/messages/:messageId', authenticateToken, deleteMessage);
+router.put('/:chatId/read', authenticateToken, markAsRead);
 
 module.exports = router;
